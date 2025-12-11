3342e7a14de69b1e007126d7399a0ea6ae21844c
NLGtan <gaborne.neithanlouisn@gmail.com>
2025-12-11 16:34:55 +0800

    fix description

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -12,7 +12,7 @@ export const projects = [
     live: "",
     repo: "https://github.com/NLGtan/kilowhatt_app",
     description:
-      "A Flutter mobile app connecting homeowners and tenants. It includes role-based dashboards, real-time bill tracking, OCR-based submeter reading, push notifications, and Supabase image uploads. The app also integrates a blockchain payment method to ensure secure, immutable bill records.",
+      "A Flutter mobile app connecting homeowners and tenants. It includes role-based dashboards, bill tracking, OCR-based submeter reading, push notifications, and Supabase image uploads. The app also integrates a blockchain payment method to ensure secure, immutable bill records.",
     stack: ["Flutter", "Firebase", "Supabase"],
     flip: false,
   },
@@ -36,7 +36,7 @@ export const projects = [
     live: "https://axari.vercel.app/",
     repo: "",
     description:
-      "A marketing‑site reimagined for the web with a Web3 focus. It blends motion, layered visuals, and responsive typography to create an immersive, modern frontend experience. Animated transitions and visual layers reinforce a sense of depth and polish, while interactive elements highlight blockchain and Web3 integration concepts. Text and layout adapt fluidly to any screen size, ensuring readability and elegance across devices. In essence: a sleek, interactive showcase built with modern frontend craftsmanship, refined UI design, and Web3-inspired interactivity.",
+      "A marketing‑site reimagined for the web with a Web3 focus. It blends motion, layered visuals, and responsive typography to create an immersive, modern frontend experience. Animated transitions and visual layers reinforce a sense of depth and polish, while interactive elements highlight blockchain and Web3 integration concepts.In essence: a sleek, interactive showcase built with modern frontend craftsmanship, refined UI design, and Web3-inspired interactivity.",
     stack: ["React", "Tailwind", "Framer Motion"],
     flip: false,
   },
